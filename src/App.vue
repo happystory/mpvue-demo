2f63164741b615855c7a509f6bd8fd48680eb2e2
liuyao <670652018@qq.com>
2018-05-26 19:26:53 +0800

    request 封装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script>
+import { get } from './util'
+
 export default {
-  created () {
+  async created () {
     console.log('start')
+
+    const res = await get('/weapp/demo')
+    console.log(1, res)
   }
 }
 </script>
