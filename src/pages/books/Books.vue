1f8a8938a2569a39335beb6bb0e79b2df7e491f8
liuyao <670652018@qq.com>
2018-05-26 19:08:59 +0800

    基本骨架

---
+++ src/pages/books/Books.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Books
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/pages/me/Me.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    个人中心页面
+    <a href="/pages/comments/main">评论</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
